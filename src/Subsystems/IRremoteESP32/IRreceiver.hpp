143d029a792da43846d3f7e67ae859766fcfc57c
RoiAttias <roiattias1@gmail.com>
2024-08-18 18:47:26 +0300

    Problem in ISR attaching inside the class.

--- src/Subsystems/IRremoteESP32/IRreceiver.hpp
+++ src/Subsystems/IRremoteESP32/IRreceiver.hpp
@@ -4,108 +4,116 @@
 #include <Arduino.h>
 
 #define NEC_BITS 32
-#define NEC_HDR_MARK 9000
-#define NEC_HDR_SPACE 4500
+#define NEC_HEADER_MARK 9000
+#define NEC_HEADER_SPACE 4500
 #define NEC_BIT_MARK 560
 #define NEC_ONE_SPACE 1690
 #define NEC_ZERO_SPACE 560
 
 typedef void (*OnReceiveHandler)(unsigned long data);
 
-class IRReceiver {
-  private:
-    int recvPin;
-    bool invert;
-    unsigned long lastTime;
-    unsigned long data;
-    int bitCount;
-    int threshold;
-    bool dataAvailable;
-    bool enable;
-    OnReceiveHandler onReceiveHandler;
+class IRReceiver
+{
+private:
+  int recvPin;
+  bool invert;
+  unsigned long lastTime;
+  unsigned long data;
+  int bitCount;
+  int threshold;
+  bool dataAvailable;
+  bool onReceiveEnable;
+  OnReceiveHandler onReceiveHandler;
 
-    static void IRAM_ATTR ISRWrapper(void *arg) {
-      IRReceiver* self = static_cast<IRReceiver*>(arg);
-      self->ISR();
+  static void IRAM_ATTR ISRWrapper(void *arg)
+  {
+    IRReceiver *self = static_cast<IRReceiver *>(arg);
+    self->decodeNec();
+  }
+
+  void decodeNec()
+  {
+    unsigned long time = micros();
+    unsigned long duration = time - lastTime;
+    lastTime = time;
+
+    if (duration >= (NEC_HEADER_MARK - threshold) && duration <= (NEC_HEADER_MARK + threshold))
+    {
+      // Detected header mark
+      bitCount = 0;
+      data = 0;
+      return;
     }
 
-    void ISR() {
-      unsigned long time = micros();
-      unsigned long duration = time - lastTime;
-      lastTime = time;
+    if (bitCount >= NEC_BITS)
+    {
+      // Already received full data
+      return;
+    }
 
-      if (duration >= (NEC_HDR_MARK - threshold) && duration <= (NEC_HDR_MARK + threshold)) {
-        // Detected header mark
-        bitCount = 0;
-        data = 0;
-        return;
+    if (duration >= (NEC_BIT_MARK - threshold) && duration <= (NEC_BIT_MARK + threshold))
+    {
+      // Detected bit mark
+      if (invert)
+      {
+        duration = digitalRead(recvPin) ? duration : 0;
       }
-
-      if (bitCount >= NEC_BITS) {
-        // Already received full data
-        return;
+      if (duration >= (NEC_ONE_SPACE - threshold) && duration <= (NEC_ONE_SPACE + threshold))
+      {
+        data = (data << 1) | 1;
       }
-
-      if (duration >= (NEC_BIT_MARK - threshold) && duration <= (NEC_BIT_MARK + threshold)) {
-        // Detected bit mark
-        if (invert) {
-          duration = digitalRead(recvPin) ? duration : 0;
-        }
-        if (duration >= (NEC_ONE_SPACE - threshold) && duration <= (NEC_ONE_SPACE + threshold)) {
-          data = (data << 1) | 1;
-        } else if (duration >= (NEC_ZERO_SPACE - threshold) && duration <= (NEC_ZERO_SPACE + threshold)) {
-          data = (data << 1);
-        }
-        bitCount++;
-
-        if (bitCount == NEC_BITS) {
-          dataAvailable = true;
-          OnReceive_execute();
-        }
+      else if (duration >= (NEC_ZERO_SPACE - threshold) && duration <= (NEC_ZERO_SPACE + threshold))
+      {
+        data = (data << 1);
       }
-    }
+      bitCount++;
 
-  public:
-    IRReceiver(int pin, int threshold = 200, bool invertSignal = false)
-      : recvPin(pin), threshold(threshold), invert(invertSignal), lastTime(0), data(0), bitCount(0), dataAvailable(false), onReceiveHandler(nullptr) {
-      pinMode(recvPin, INPUT);
-      if (invert) {
-        attachInterruptArg(digitalPinToInterrupt(recvPin), ISRWrapper, this, FALLING);
-      } else {
-        attachInterruptArg(digitalPinToInterrupt(recvPin), ISRWrapper, this, RISING);
-      }
-    }
-
-    bool available() {
-      return dataAvailable;
-    }
-
-    unsigned long read() {
-      dataAvailable = false;
-      return data;
-    }
-
-    void OnReceive_enable(bool enable) {
-      if (enable) {
-        if (invert) {
-          attachInterruptArg(digitalPinToInterrupt(recvPin), ISRWrapper, this, FALLING);
-        } else {
-          attachInterruptArg(digitalPinToInterrupt(recvPin), ISRWrapper, this, RISING);
-        }
-      } else {
-        detachInterrupt(digitalPinToInterrupt(recvPin));
+      if (bitCount == NEC_BITS)
+      {
+        dataAvailable = true;
+        OnReceive_execute();
       }
     }
+  }
 
-    void OnReceive_setHandler(OnReceiveHandler handler) {
-      onReceiveHandler = handler;
-    }
+public:
+  IRReceiver(int pin, bool invertSignal = true, int threshold = 200)
+      : recvPin(pin), invert(invertSignal), threshold(threshold),
+        lastTime(0), data(0), bitCount(0), 
+        dataAvailable(false), onReceiveHandler(nullptr), onReceiveEnable(false)
+  {
+    pinMode(recvPin, INPUT);
+    attachInterruptArg(digitalPinToInterrupt(recvPin), ISRWrapper, this, invert ? FALLING : RISING);
+  }
 
-    void OnReceive_execute() {
-      if (dataAvailable && onReceiveHandler && enable) {
-        onReceiveHandler(data);
-      }
+  bool available()
+  {
+    return dataAvailable;
+  }
+
+  unsigned long read()
+  {
+    dataAvailable = false;
+    return data;
+  }
+
+  void OnReceive_enable(bool enable)
+  {
+    onReceiveEnable = enable;
+  }
+
+  void OnReceive_setHandler(OnReceiveHandler handler)
+  {
+    onReceiveHandler = handler;
+  }
+
+  void OnReceive_execute()
+  {
+    if (dataAvailable && onReceiveHandler && onReceiveEnable)
+    {
+      onReceiveHandler(data);
     }
+  }
 };
 
 #endif // IRRECEIVER_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,40 @@
 #include <Arduino.h>
-
+/**
 #include "Constants.h"
 #include "Utilities\HyperList.hpp"
-//#include "Subsystems\TGUI\TGUI.hpp"
+//#include "Subsystems\TGUI\TGUI.hpp"*/
 #include "Subsystems\IRremoteESP32\IRremoteESP32.hpp"
 
-IRReceiver irReceiver(14, 200, false); // Pin 14, threshold 200, no invert
+IRReceiver irReceiver(18,true); // Pin 14, threshold 200, invert
 
 void onReceive(unsigned long data) {
   Serial.print("Received NEC command: 0x");
   Serial.println(data, HEX);
 }
-
+void fun(void *arg){
+  int *x = static_cast<int *>(arg);
+  Serial.println("Success!");
+}
+int num = 0;
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200);/* 
   irReceiver.OnReceive_setHandler(onReceive);
-  irReceiver.OnReceive_enable(true);
+  irReceiver.OnReceive_enable(true);*/
+  /*
+  esp_err_t err = gpio_install_isr_service(0);
+    if (err != ESP_OK) {
+        Serial.printf("Failed to install ISR service: %d\n", err);
+    } else {
+        Serial.println("ISR service installed successfully.");
+    }
+    */
 }
 
-void loop() {
+void loop() {/*
   // Optionally handle received data here if needed
   if (irReceiver.available()) {
     unsigned long result = irReceiver.read();
     Serial.print("Received NEC command: 0x");
     Serial.println(result, HEX);
-  }
+  }*/
 }
