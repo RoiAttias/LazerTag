150cf39796fe815608a1ed8ca51115c0c5d05ffc
RoiAttias <roiattias1@gmail.com>
2024-09-04 09:41:08 +0300

    there're might be some changes. NeoPixel and OLED tests worked.

--- src/codes/vest.cpp
+++ src/codes/vest.cpp
@@ -1,5 +1,4 @@
 /*
-
 #include <Arduino.h>
 
 #include "Constants.h"
@@ -10,29 +9,25 @@
 
 void IRAM_ATTR handleDecoding();
 // add channels to handleDecoding to pass it as arg
-IRreceiver recv(18,handleDecoding);
+IRreceiver recv(18);//,handleDecoding);
 
 void IRAM_ATTR handleDecoding()
 {
   recv.decodeNEC();
 }
 
-void handleReceivedData(unsigned long data) {
-  Serial.print("Received data: ");
-  Serial.println(data, HEX);  // Print the received data in hexadecimal format
-}
-
 void setup() {
   Serial.begin(115200);  // Initialize Serial for output
   //pinMode(recvPin, INPUT_PULLUP);
-  //attachInterrupt(digitalPinToInterrupt(recvPin), decodeNEC, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(18), handleDecoding, CHANGE);
   //lastTime = micros();
 }
 
 void loop() {
   if(recv.available())
   {
-    Serial.println(recv.read());
+    //Serial.print('!');
+    Serial.println(recv.read(),HEX);
   }
 }
 
